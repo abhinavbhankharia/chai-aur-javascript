ea22a0c6c9b714b10a6be8c2fdffe340274fa460
abhinavbhankharia <abhinavbhankharia@gmail.com>
2024-10-25 22:03:05 +0530

    datatypes

--- 01_Basics/01_Variables.js
+++ 01_Basics/01_Variables.js
@@ -2,7 +2,7 @@ const accountID = 123453
 let accountEmail = "abhinav@gmail.com"
 var accountPassword = "123456"
 accountCity = "Jaipur" //unsafe method to resrve memory withour declaring let or const
-let accountState       //variable is declared with no value thn it is considered as not defined
+let accountState       //variable is declared with no value, then it is considered as not defined
 
 // accountId = 2 changing const values not allowed
 
