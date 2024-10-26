e2f80c62915b38244f395d70f19c176604ee0f0e
abhinavbhankharia <abhinavbhankharia@gmail.com>
2024-10-26 21:48:01 +0530

    Learning Operations

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -29,3 +29,36 @@ let someNumber = 33
 let StringNumber = String(someNumber)
 console.log(StringNumber);
 console.log(typeof StringNumber);
+
+//*********      OPERATIONS      **********
+
+let value = 3
+let negValue = -3
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3); // power
+console.log(3/2);
+console.log(3%2);
+
+let str1 = "hello"
+let str2 = " abhinav"
+let str3 = str1 + str2
+console.log(str3);  //simple string addition, subtaction not possible
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 1 + 2);
+console.log(1 + 1 + "2");
+
+console.log(+true); //this gives 1 as boolean value
+console.log(+"");   //this gives 0 as boolean value
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2      //bad code readability
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
